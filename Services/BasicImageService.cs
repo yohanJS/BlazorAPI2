1824daa2e32c0201136b8ad64916ee304389bc52
Yohan Garcia <yoanvaldes03@gmail.com>
2023-12-21 20:28:43 -0500

    Add project files.

---
+++ Services/BasicImageService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace WebApiPoc.Services
+{
+    public class BasicImageService : IImageService
+    {
+        public string ConvertByteArrayToFile(byte[] fileData, string extension)
+        {
+            if (fileData is null) return string.Empty;
+
+            string imageBase64Data = Convert.ToBase64String(fileData);
+            return $"data:{extension};base64,{imageBase64Data}";
+        }
+
+        public async Task<List<byte[]>> ConvertFilesToByteArrayAsync(List<IFormFile> files)
+        {
+            List<byte[]> byteFiles = new List<byte[]>();
+
+            foreach (var file in files)
+            {
+                using MemoryStream memoryStream = new();
+                await file.CopyToAsync(memoryStream);
+                byte[] byteFile = memoryStream.ToArray();
+                byteFiles.Add(byteFile);
+            }
+
+            return byteFiles;
+        }
+    }
+}
---
+++ Services/IImageService.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace WebApiPoc.Services
+{
+    public interface IImageService
+    {
+        Task<List<byte[]>> ConvertFilesToByteArrayAsync(List<IFormFile> files);
+        string ConvertByteArrayToFile(byte[] fileData, string extension);
+
+    }
+}
